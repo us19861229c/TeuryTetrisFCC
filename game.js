1b50eaa9e37daa056650e5557ca988f8a0a0a3de
Rebeca Santos <rcks.santos@gmail.com>
2020-06-13 13:58:31 -0300

    cria variaveis pertinentes a estrutura inicial da tela do jogo

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
-document.addEventListener('DOMContentLoader', () => ) {
-  
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const width = 10
+
+  const scoreDisplay = document.querySelectorAll('#score')
+  const startButton = document.querySelector('#start-button')
+})
