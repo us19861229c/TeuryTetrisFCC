d2a838c5c87b3a0cdc29c2e9c4a4d71c14b26e99
Rebeca Santos <rcks.santos@gmail.com>
2020-06-13 14:12:03 -0300

    cria as peças de Tetris com suas rotações

--- game.js
+++ game.js
@@ -5,4 +5,43 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const scoreDisplay = document.querySelectorAll('#score')
   const startButton = document.querySelector('#start-button')
+
+// as peças de Tetris
+  //Peça em L
+  const pEmL = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+  //Peça em Z
+  const pEmZ = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+  //Peça em T
+  const pEmT = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+  //Peça em O
+  const pEmO = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+  //Peça em I
+  const pEmI = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
+  
 })
