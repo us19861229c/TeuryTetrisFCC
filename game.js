17f95a491a12cac99597be056ad4fdcaba108399
Rebeca Santos <rcks.santos@gmail.com>
2020-06-19 13:09:30 -0300

    aplica o relacionamento entre o teclado e a movimentação das peças

--- game.js
+++ game.js
@@ -46,17 +46,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const pecasTetris = [pEmL, pEmZ, pEmO, pEmT, pEmI]
 
   let posAtual = 4
-  let rotaAtual = 0
+  let rotacaoAtual = 0
   //escolher aleatoriamente uma peça de Tetris
   let randomico = Math.floor(Math.random()*pecasTetris.length)
-  let atual = pecasTetris[randomico][rotaAtual]
+  let atual = pecasTetris[randomico][rotacaoAtual]
 
   //desenha a peça de Tetris
   function desenhar() {
     atual.forEach(index => {
       squares[posAtual + index].classList.add('pTetris')
     })
-  }  
+  }
 
   //apaga a peça de Tetris
   function apagar() {
@@ -68,12 +68,77 @@ document.addEventListener('DOMContentLoaded', () => {
   //faz a peça de Tetris mover-se para baixo a cada segundo 
   timerId = setInterval(moveParaBaixo, 1000)
 
-    //move a peça para baixo
-    function moveParaBaixo() {
-      apagar()
-      posAtual += width
+  //move a peça naturalmente para baixo com o passar do tempo
+  function moveParaBaixo() {
+    apagar()
+    posAtual += width
+    desenhar()
+    pecaPresa()
+  }
+
+  //função que prende a peça ao chão
+  function pecaPresa(){
+    if(atual.some(index => squares[posAtual + index + width].classList.contains('usada'))) {
+      atual.forEach(index => squares[posAtual + index].classList.add('usada'))
+      //gera uma nova peça randômica
+      randomico = Math.floor(Math.random() * pecasTetris.length)
+      atual = pecasTetris[randomico][rotacaoAtual]
+      posAtual = 4
       desenhar()
     }
+  }
+
+  //relaciona as teclas do teclado aos movimentos da peça (esquerda, direita, para baixo mais rápido, para cima gira a peça)
+  function controlaTecladoMovimento(e) {
+    if(e.keyCode === 37) {
+      moveParaEsquerda()
+    }else if (e.keyCode === 38){
+      rotacionaAPeca()
+    }else if (e.keyCode === 39){
+      moveParaDireita()
+    }else if (e.keyCode === 40){
+      moveParaBaixo()
+    }
+  }
+  document.addEventListener('keyup', controlaTecladoMovimento)
+
+  //rotaciona a peça de Tetris
+  function rotacionaAPeca() {
+    apagar()
+    rotacaoAtual++
+    if(rotacaoAtual === atual.length) { //se a rotação atingir o n 4 , faz ela zerar e recomeçar
+      rotacaoAtual = 0
+    }
+    atual = pecasTetris[randomico][rotacaoAtual]
+    desenhar()
+  }
+
+  //move a peça de Tetris para a esquerda até encontrar os limites da fase. 
+  function moveParaEsquerda() {
+    apagar()
+    const estaNaBordaEsquerda = atual.some(index => (posAtual + index) % width === 0)
+
+    if(!estaNaBordaEsquerda) posAtual -=1
+
+    if(atual.some(index => squares[posAtual + index].classList.contains('usada'))) {
+      posAtual +=1
+    }
+
+    desenhar()
+  }
+  //move a peça de Tetris para a direita até encontrar os limites da fase. 
+  function moveParaDireita() {
+    apagar()
+    const estaNaBordaDireita = atual.some( index => (posAtual + index) % width === width -1)
+
+    if(!estaNaBordaDireita) posAtual +=1
+
+    if(atual.some(index => squares[posAtual + index].classList.contains('usada'))) {
+      posAtual -=1
+    }
+
+    desenhar()
+  }
 
 
 })
